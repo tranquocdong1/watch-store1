b0681fcfebcfa73d23a8dc299347495089df39d1
tranquocdong1 <tranquocdong10.2@gmail.com>
2024-12-17 11:25:16 +0700

    add swagger

---
+++ src/main/java/com/example/watchstore/swagger.java
@@ -0,0 +1,21 @@
+package com.example.watchstore;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class swagger {
+
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User API Documentation")
+                        .version("3.0")
+                        .description("API documentation for managing users"));
+    }
+}
